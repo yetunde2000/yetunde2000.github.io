953e1621e1ac5376c2864788f5eca93932c7022f
obasi yetunde <olaopin_5@dgist.ac.kr>
2025-12-31 10:40:24 +0900

    I made chnages

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,58 +8,50 @@ export interface Experience {
 }
 
 export const experienceData: Experience[] = [
-  {
-    role: "Work & Internship",
-    experiences: [
-      {
-        year: "Jan 2024 – Feb 2024",
-        title: "Research Intern (Mentor: Duri Lee, Advisor: Uichin Lee)",
-        institution: "KAIST [ICLab](https://iclab.kaist.ac.kr/, color=blue)"
-      },
-      {
-        year: "Feb 2022 – Feb 2023",
-        title: "Data Analysis Intern Researcher",
-        institution: "[INTERX](https://interxlab.com/, color=dark_brown) in Ulsan, Korea"
-      },
-      {
-        year: "Feb 2021 – Aug 2021",
-        title: "Data Analyst and UI/UX Designer",
-        institution: "Travel Startup in Ulsan, Korea"
-      }
-    ]
-  },
+
   {
     role: "Teaching & Academic Support",
     experiences: [
       {
-        year: "2025 Spring",
-        title: "Teaching Assistant, CS565_DS522 IoT Data Science",
-        institution: "KAIST"
+        year: "January 2025 - December 2025",
+        title: "DGIST International student association (DISA) PR & Media Team ",
+        institution: "DGIST"
       },
       {
-        year: "Jul 2022 – Oct 2022",
-        title: "Student Supporter, Starmooc Lecture Video Production",
-        institution: "UNIST"
+        year: "June 2025 - August 2025",
+        title: "Mentored intern students for research projects in AI ",
+        institution: "DGIST"
+      },
+      {
+        year: "January 2024 - December 2024",
+        title: "DGIST International student association (DISA) PR & Media Team ",
+        institution: "DGIST"
+      },
+      {
+        year: "August 2024 - December 2024",
+        title: "Teaching Assistant(TA) for physics course",
+        institution: "DGIST"
+      },
+      {
+        year: "Nov 2022 – Oct 2023",
+        title: "National Youth Service Corps (NYSC); Physics Tutor",
+        institution: "BenGee Model College, Agege, Lagos State, Nigeria"
       }
     ]
   },
   {
     role: "Mentorship & Outreach",
     experiences: [
+
       {
-        year: "Fall 2024, Summer 2025",
-        title: "Mentor, Research Internship",
-        institution: "KAIST ICLab"
+        year: "Nov 2022 – Oct 2023",
+        title: "National Youth Service Corps (NYSC); Physics Tutor",
+        institution: "BenGee Model College, Agege, Lagos State, Nigeria"
       },
       {
-        year: "Jan 2022",
-        title: "Mentor, Explore@UNIST – High School Science & Tech Leadership Camp",
-        institution: "UNIST"
-      },
-      {
-        year: "Apr 2021 – Oct 2021",
-        title: "Mentor, Club-to-Club Entrepreneurship Mentorship for High School Students",
-        institution: "UNIST"
+        year: "Nov 2022 – April 2023",
+        title: "Operation Feed; Volunteer Mentor",
+        institution: "Inspired Smiles Foundation, Lagos, Nigeria"
       }
     ]
   }
